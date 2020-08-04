4f83e8b85c77ba44d497f03c17599d44cc8d6516
stoodleyc <stoodleyc@stu.otc.school.nz>
2020-08-05 11:00:42 +1200

    i did a cool thing

--- black jack.py
+++ black jack.py
@@ -1,6 +1,36 @@
+#setting up things that are constant
+from random import randint
+def tell_hand():
+    print("your hand is... {}!".format(Hand))
+
+def player_turn():
+    print("it is now your turn")
+    tell_hand()
+    print("would you like to hit or stand?")
+    playermove=input()
+
+
+
 #start screen
-print("█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
-█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
-█░░║║║╠─║─║─║║║║║╠─░░█
-█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
-█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█")
+print('''▀█████████▄   ▄█          ▄████████  ▄████████    ▄█   ▄█▄      ▄█    ▄████████  ▄████████    ▄█   ▄█▄ 
+  ███    ███ ███         ███    ███ ███    ███   ███ ▄███▀     ███   ███    ███ ███    ███   ███ ▄███▀ 
+  ███    ███ ███         ███    ███ ███    █▀    ███▐██▀       ███   ███    ███ ███    █▀    ███▐██▀   
+ ▄███▄▄▄██▀  ███         ███    ███ ███         ▄█████▀        ███   ███    ███ ███         ▄█████▀    
+▀▀███▀▀▀██▄  ███       ▀███████████ ███        ▀▀█████▄        ███ ▀███████████ ███        ▀▀█████▄    
+  ███    ██▄ ███         ███    ███ ███    █▄    ███▐██▄       ███   ███    ███ ███    █▄    ███▐██▄   
+  ███    ███ ███▌    ▄   ███    ███ ███    ███   ███ ▀███▄     ███   ███    ███ ███    ███   ███ ▀███▄ 
+▄█████████▀  █████▄▄██   ███    █▀  ████████▀    ███   ▀█▀ █▄ ▄███   ███    █▀  ████████▀    ███   ▀█▀ 
+             ▀                                   ▀         ▀▀▀▀▀▀                            ▀         
+hello there, welcome to blackjack.
+for help type help anything else will just start the game''')
+start=input()
+if start == "help":
+    print("[I can't be bothered typing an explanation right now so I'll do it later]")
+#setting up the game
+Hand = randint(2,20)
+DealerHand = randint(2,20)
+
+
+
+#game starts here
+player_turn()
