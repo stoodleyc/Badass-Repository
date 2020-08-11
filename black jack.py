2615bc353ff86f28d609f7da8d76d9b0cfbbc5f6
stoodleyc <stoodleyc@stu.otc.school.nz>
2020-08-11 13:29:52 +1200

    i did a thing

--- black jack.py
+++ black jack.py
@@ -2,7 +2,9 @@
 from random import randint
 Hand = 0
 DealerHand = 0
-help = ("[I can't be bothered typing an explanation right now so I'll do it later]")
+unknown = ('''
+sorry I don't recognize that answer please try again
+''')
 
 #functions and stuff
 def tell_hand():
@@ -10,10 +12,26 @@ def tell_hand():
     print("your hand is... {}!".format(Hand))
 
 def player_turn():
+    global unknown
     print("it is now your turn!")
     tell_hand()
     print("would you like to hit or stand?")
     playermove=input()
+    if playermove == "hit":
+        hit()
+    elif playermove == "Hit":
+        hit()
+    elif playermove == "HIT":
+        hit()
+    elif playermove == "stand":
+        stand()
+    elif playermove == "Stand":
+        stand()
+    elif playermove == "STAND":
+        stand()
+    else:
+        print (unknown)
+        player_turn()
 
 
 def round():
@@ -21,12 +39,25 @@ def round():
     global Hand
     Hand = randint(2,20)
     DealerHand = randint(2,20)
-
     player_turn()
+
+def hit():
+    global Hand
+    Hand = Hand + randint(1,10)
+    tell_hand()
+
+def stand():
+    print("bruh")
 
 def Game():
     score = 0
     round()
+
+def help():
+    print("[I can't be bothered typing an explanation right now so I'll do it later]")
+    print('''
+    type anything to start!''')
+    input()
 
 #start screen
 print('''▀█████████▄   ▄█          ▄████████  ▄████████    ▄█   ▄█▄      ▄█    ▄████████  ▄████████    ▄█   ▄█▄ 
@@ -42,11 +73,12 @@ hello there, welcome to blackjack.
 for help type help anything else will just start the game''')
 start=input()
 if start == "help":
-    print(help)
+    help()
 elif start == "Help":
-    print(help)
+    help()
 elif start == "HELP":
-    print (help)
+    help()
 
-#starting the game
+#starting
+# the game
 Game()
